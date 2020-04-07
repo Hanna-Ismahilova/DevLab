86ffc70d70a3f924a9df4073fdd2cc85f14e5676
Hanna-Ismahilova <hi.infoshare@gmail.com@gmail.com>
2020-04-07 09:32:55 +0200

    Init commit

---
+++ DevLabApp/DevLabApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevLabApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
